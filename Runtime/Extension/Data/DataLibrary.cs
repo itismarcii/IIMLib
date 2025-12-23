23b57f259c7c4b9a2c38f97c2dff47f8e867cc98
itismarcii <itismarcii@itismarcii-dev.com>
2025-12-23 22:26:20 +0100

    UPDATED - Clean-up

--- Runtime/Extension/Data/DataLibrary.cs
+++ Runtime/Extension/Data/DataLibrary.cs
@@ -8,26 +8,29 @@ namespace IIMLib.Extension.Data
         private readonly Dictionary<Type, IDataEntry> _DataEntries = new();
         
         public T Get<T>() where T : DataEntry<T> => (T)_DataEntries[typeof(T)].CloneUntyped();
+        public T GetSave<T>() where T : DataEntry<T> =>  _DataEntries.TryGetValue(typeof(T), out var entry) ? (T)entry.CloneUntyped() : null;
         public T GetOrigin<T>() where T : DataEntry<T> => (T)_DataEntries[typeof(T)];
-        public T GetOrDefault<T>() where T : DataEntry<T>, new() => _DataEntries.TryGetValue(typeof(T), out var entry) ? entry as T : new T();
+        public T GetOrDefault<T>() where T : DataEntry<T>, new() => _DataEntries.TryGetValue(typeof(T), out var entry) ? entry.CloneUntyped() as T : new T();
         public void ClearAll() =>  _DataEntries.Clear();
-        public void Clear<T>() where T : DataEntry<T>, new() => _DataEntries.Remove(typeof(T));
-        public T GetCleanData<T>() where T : DataEntry<T>, new() => GetOrDefault<T>().CreateCleanDataEntry() as T;
+        public void Clear<T>() => _DataEntries.Remove(typeof(T));
+        public T GetCleanData<T>() where T : DataEntry<T>, new() => _DataEntries.TryGetValue(typeof(T), out var entry) ? ((T)entry).CreateCleanDataEntry() : new T().CreateCleanDataEntry();
 
         public T Update<T>(T data) where T : DataEntry<T>, new()
         {
-            if (data == null) return null;
-
+            if (data == null) throw new ArgumentNullException(nameof(data));
+            
             if (_DataEntries.TryGetValue(typeof(T), out var entry))
             {
                 ((T)entry).Merge(data);
-                return Get<T>();
             }
-
-            var instance = new T();
-            instance.Merge(data);
-            _DataEntries[typeof(T)] = instance;
-            return instance;
+            else
+            {
+                var instance = new T();
+                instance.Merge(data);
+                _DataEntries[typeof(T)] = instance;
+            }
+            
+            return Get<T>();
         }
 
         public void Merge(DataLibrary library)
